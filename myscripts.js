7bddb5c0e6dce0a5b6bcbf5889ed4a263e43294c
agenttuy <agenttuy@gmail.com>
2017-02-13 14:32:44 -0500

    my first commit

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,5 @@
+
+
 var operand1;
 var operand2;
 
@@ -21,6 +23,11 @@ function checkAnswer(){
   }
 }
 
+
+/**
+  JavaScript for the gallery
+
+**/
 var pics = [
               "https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg",
               "http://static.boredpanda.com/blog/wp-content/uploads/2016/10/worlds-most-beautiful-cats-52-57fc9cb29d61b__700.jpg",
